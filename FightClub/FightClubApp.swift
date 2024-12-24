ecb4a6dc5038fc2014f31c902b15e487d9be0f82
goplayzig <goplayzig@gmail.com>
2024-12-25 00:11:17 +0900

    Initial Commit

---
+++ FightClub/FightClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FightClubApp.swift
+//  FightClub
+//
+//  Created by Edward Lee on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FightClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
