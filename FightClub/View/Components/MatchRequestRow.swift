6811cf9ca784156824d2b9599fac1ceb0cc9f228
goplayzig <goplayzig@gmail.com>
2024-12-27 01:31:52 +0900

    add MatchRequestRow

---
+++ FightClub/View/Components/MatchRequestRow.swift
@@ -0,0 +1,18 @@
+//
+//  MatchRequestRow.swift
+//  FightClub
+//
+//  Created by Edward Lee on 12/27/24.
+//
+
+import SwiftUI
+
+struct MatchRequestRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchRequestRow()
+}
