7bc0b81bf47e872795f7f02ecfca6dc505a2f1a1
jihoon42 <jerryk100@g.hongik.ac.kr>
2025-01-06 20:57:22 +0900

    Update Profile.view and get 200 response

--- FightClub/API/API.swift
+++ FightClub/API/API.swift
@@ -9,18 +9,21 @@ import Alamofire
 
 enum APIEndpoint {
     case logIn(email: String, provider: String, token: String)
+    case signup(email: String, provider: String, token: String) // signup 추가
     
     // 엔드 포인트
     var url: String {
         switch self {
         case .logIn:
-            return "http://3.34.46.87:8080/login"
+            return "http://3.34.46.87:8080/login" // 기존 로그인 URL
+        case .signup:
+            return "http://3.34.46.87:8080/signup" // 새로운 회원가입 URL
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .logIn:
+        case .logIn, .signup: // 둘 다 POST 메서드 사용
             return .post
         }
     }
@@ -28,9 +31,12 @@ enum APIEndpoint {
     var parameters: Parameters? {
         switch self {
         case .logIn(let email, let provider, let idToken):
-            print(["email": email, "provider": provider, "idToken": idToken])
+            print("Sending logIn parameters:", ["email": email, "provider": provider, "idToken": idToken])
+            return ["email": email, "provider": provider, "idToken": idToken]
+            
+        case .signup(let email, let provider, let idToken): // signup의 매개변수 추가
+            print("Sending register parameters:", ["email": email, "provider": provider, "idToken": idToken])
             return ["email": email, "provider": provider, "idToken": idToken]
         }
     }
 }
-
